5708c00436dab1689a58736d01a19d556812cb5a
elilcatness <elilcatness@gmail.com>
2021-07-27 02:21:11 +0300

    dsajdfajsfjasfsafadsadsada

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import time
 
@@ -46,5 +46,5 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv()
+    # load_dotenv()
     main()
